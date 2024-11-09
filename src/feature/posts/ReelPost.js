82a5cda9b2b4c49746e4c35ecbc385cd32cc3b91
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-11-09 02:07:55 +0100

    commit

--- src/feature/posts/ReelPost.js
+++ src/feature/posts/ReelPost.js
@@ -93,7 +93,7 @@ const reelcontent = <>
         <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
     </button>
-    <button className="next fixed rounded-full bg-white text-black left-72 -translate-x-36 sm:left-96 sm:translate-x-32 flex flex-col items-center justify-center" onClick={() => setIndex(index + 1)}>
+    <button className="next fixed rounded-full bg-white text-black left-72 -translate-x-16 sm:left-96 sm:translate-x-32 flex flex-col items-center justify-center" onClick={() => setIndex(index + 1)}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-black">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
       </svg>
@@ -102,8 +102,7 @@ const reelcontent = <>
 </>
 
 return(
-    <section className="flex flex-row justify-between bg-black items-center">
-    <SidePosts />
+    <section >
     <div>
     {reelcontent}
      </div>
